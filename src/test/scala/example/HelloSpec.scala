f822f1fbd5b46d893a8ef8476fa76295b31e7a62
juan <juanan_rp@hotmail.co.uk>
2018-05-18 15:05:27 +0100

    starting kata

--- src/test/scala/example/HelloSpec.scala
+++ src/test/scala/example/HelloSpec.scala
@@ -3,11 +3,49 @@ package example
 import org.scalatestplus.play.PlaySpec
 
 class HelloSpec extends PlaySpec  {
-  "The Hello object" should {
-    "Test" when {
-      "say hello" in {
-        Hello.greeting mustBe("hello")
-      }
+
+
+  "A world" should {
+
+    val world  = World(10,10)
+
+    "contain a single ant positioned in the middle" in {
+        world.ant.antPosition mustBe (5,5)
+    }
+    "contain a single ant facing north" in {
+      world.ant.antDirection mustBe North
+    }
+    "contain an ant that can move forward" in {
+      world.ant.move mustBe Ant(6, 5, North)
     }
   }
 }
+
+case class World(width: Int, height: Int, ant: Ant) {
+
+}
+
+case class Ant(x: Int, y: Int, orientation: Orientation) {
+
+  def antPosition: (Int, Int) = (x, y)
+  def antDirection: Orientation = North
+
+  def move: Ant = this.copy(x + 1, y, orientation)
+
+}
+
+object World {
+  def apply(width: Int, height: Int): World = World(width, height, Ant())
+}
+
+object Ant {
+  def apply() : Ant = Ant(5,5,North)
+}
+
+sealed trait Orientation
+
+case object North extends Orientation
+case object South extends Orientation
+case object East extends Orientation
+case object West extends Orientation
+
